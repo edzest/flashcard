aa1ce901737b6ca5c129b03a3ad55c418a597e83
Piyush Ranjan <piyushranjan95@gmail.com>
2024-12-27 20:44:19 +0530

    settings icon in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './Navbar.css';
 
 function Navbar() {
+
+  const handleSettingsClick = () => {
+    window.location.href = '/settings';
+  };
+
+  const handleHomeClick = () => {
+    window.location.href = '/';
+  }
+
   return (
     <nav className="navbar">
       <div className='container'>
-      <h1 className="navbar-title">FlashCards <span className='edzest'>by edzest</span></h1>
+      <h1 className="navbar-title" onClick={handleHomeClick}>FlashCards <span className='edzest'>by edzest</span></h1>
+      <span className="settings-icon" onClick={handleSettingsClick}>&#9881;</span>
       </div>
     </nav>
   );
